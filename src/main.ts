02a3affa3031ba8f43971b8d6f25d2fb9afc58b4
Kenan-Aliev <kenan199948@gmail.com>
2022-01-14 14:13:47 +0600

    make it better

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import * as cookieParser from 'cookie-parser'
 
-// const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 async function start() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
     app.enableCors({credentials: true})
-    await app.listen(process.env.PORT, () => {
-        console.log(`Сервер запущен на порту ${process.env.PORT}`)
+    await app.listen(PORT, () => {
+        console.log(`Сервер запущен на порту ${PORT}`)
     });
 }
 
