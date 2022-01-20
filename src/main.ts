d91d82498d5ffb7af79228067da21a5c76181255
Kenan-Aliev <kenan199948@gmail.com>
2022-01-20 21:40:51 +0600

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser'
 
 const PORT = Number(process.env.PORT) || 5000
@@ -8,7 +8,12 @@ const PORT = Number(process.env.PORT) || 5000
 async function start() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
-    app.enableCors({credentials: true})
+    app.enableCors(
+        {
+            credentials: true,
+            origin: process.env.CLIENT_URL
+        }
+    )
     const config = new DocumentBuilder()
         .setTitle('CV-DEV API')
         .setDescription('Cv-Dev Api Documentation')
